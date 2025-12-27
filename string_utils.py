1fd1de73cdab83c7be4902b93ba28bc0447b4990
maayan-wag <maayan.wagshal@gmail.com>
2025-12-27 18:12:32 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,26 +1,26 @@
-def  split_by_capitals(molecular_formula):
-  if not molecular_formula:
+def  split_before_uppercases(formula):
+  if not formula:
     return[]
   result = []
   start = 0
   end = 1
-  for i in molecular_formula[1:]:
+  for i in formula[1:]:
     if i.isupper():
-      result.append(molecular_formula[start:end])
+      result.append(formula[start:end])
       start = end
     end += 1
-  result.append(molecular_formula[start:])
+  result.append(formula[start:])
   return result
 
-def split_at_number(atom):
+def split_at_digit(element_string):
     digit_location = 1
-    for chr in atom[1:]:
+    for chr in element_string[1:]:
       if chr.isdigit():
         break
       digit_location +=1
-    if digit_location == len(atom):
-      return atom,1
-    return atom[:digit_location],int(atom[digit_location:])
+    if digit_location == len(element_string):
+      return element_string,1
+    return element_string[:digit_location],int(element_string[digit_location:])
 
 def count_atoms_in_molecule(molecular_formula):
     """Takes a molecular formula (string) and returns a dictionary of atom counts.  
@@ -28,8 +28,8 @@ def count_atoms_in_molecule(molecular_formula):
 
     atom_and_count={}
 
-    for atom in split_by_capitals(molecular_formula):
-        atom_name, atom_count = split_at_number(atom)
+    for atom in split_before_uppercases(formula):
+        atom_name, atom_count = split_at_digit(atom)
         if atom_name in atom_and_count:
           atom_and_count[atom_name]+=atom_count
         else:
